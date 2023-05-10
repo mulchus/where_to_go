6e3cca9c95b690007993335b935d39335aa22eaa
mulchus <mulch@list.ru>
2023-05-10 17:29:26 +0300

    Add Place model. Add Image model. (don't good)

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Place, Image
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lng', 'lat')
+
+
+class ImageAdmin(admin.ModelAdmin):
+    # list_display = ('title', 'place', )
+    pass
 
 # Register your models here.
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Image, ImageAdmin)
--- places/models.py
+++ places/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
-# Create your models here.
-print("hello!")
+
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    lng = models.FloatField()
+    lat = models.FloatField()
+    # DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    sequence_number = models.AutoField(auto_created=True, primary_key=True, serialize=False,
+                                       verbose_name='ID')
+    title = models.CharField(max_length=200, default='no title')
+    image = models.ImageField()
+    place = models.ForeignKey('Place', related_name='images', null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.sequence_number} {self.title} {self.place}'
