f39203f40a7ecc8e4afd80383a4aa59fa9c60fc3
mulchus <mulch@list.ru>
2023-05-11 15:50:32 +0300

    Move data from template to view (don't good)

--- places/models.py
+++ places/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
+    short_title = models.CharField(max_length=30)
+    placeId = models.CharField(max_length=30)
     description_short = models.TextField()
     description_long = models.TextField()
     lng = models.FloatField()
     lat = models.FloatField()
-    # DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
 
     def __str__(self):
         return self.title
--- places/views.py
+++ places/views.py
@@ -1,9 +1,47 @@
+import os
 from django.shortcuts import render
 from .models import Place, Image
+# from pathlib import Path
+from where_to_go.settings import BASE_DIR
+
+
+# BASE_DIR = Path(__file__).resolve()
 
 
 def start(request):
-    places = Place.objects.all()
-    # places_serialization =
-    print(places)
-    return render(request, 'index.html')
+    places = []
+    for place in Place.objects.all():
+        images = []
+        for image in place.images.all():
+            # images.append(os.path.join(BASE_DIR, image.image.url[1:]))
+            images.append(os.path.join('C:/PythonProjects/where_to_go/', image.image.url[1:]))
+        one_place = {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [place.lng, place.lat]
+            },
+            'properties': {
+                'title': place.short_title,
+                'placeId': place.placeId,
+                'detailsUrl': {
+                    "title": place.title,
+                    "imgs": images,
+                    "description_short": place.description_short,
+                    "description_long": place.description_long,
+                    "coordinates": {
+                        "lng": str(place.lng),
+                        "lat": str(place.lat)
+                    }
+                }
+            }
+        }
+        places.append(one_place)
+    all_places = {
+        'type': 'FeatureCollection',
+        'features': places
+    }
+
+
+    print(all_places)
+    return render(request, 'index.html', {'all_places': all_places})
