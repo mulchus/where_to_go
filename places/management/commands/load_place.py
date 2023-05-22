1f05272e28677f95d10a595b6ab2e39576996706
mulchus <mulch@list.ru>
2023-05-22 14:52:55 +0300

    update file save to ContentFile.  renamed var. deleted vars type transform.

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -4,7 +4,7 @@ from urllib.parse import urlparse
 from pathlib import Path
 from django.core.management.base import BaseCommand
 from places.models import Place, Image
-from where_to_go.settings import MEDIA_ROOT
+from django.core.files.base import ContentFile
 
 
 class Command(BaseCommand):
@@ -18,34 +18,31 @@ class Command(BaseCommand):
                             )
 
     def handle(self, *args, **options):
-        Path(MEDIA_ROOT).mkdir(parents=True, exist_ok=True)
         response = requests.get(options['url_about_place'])
         response.raise_for_status()
-        new_place = response.json()
-        new_place_lng = new_place['coordinates']['lng']
-        new_place_lat = new_place['coordinates']['lat']
+        row_place = response.json()
+        row_place_lng = row_place['coordinates']['lng']
+        row_place_lat = row_place['coordinates']['lat']
 
         place, place_created = Place.objects.get_or_create(
-            title=new_place['title'],
-            lng=float(new_place_lng),
-            lat=float(new_place_lat),
-            defaults={'description_short': new_place['description_short'],
-                      'description_long': new_place['description_long']},
+            title=row_place['title'],
+            lng=row_place_lng,
+            lat=row_place_lat,
+            defaults={'description_short': row_place['description_short'],
+                      'description_long': row_place['description_long']},
         )
 
         if not place_created:
-            print(f'Место "{new_place["title"]}" уже есть в базе данных.')
+            print(f'Место "{row_place["title"]}" уже есть в базе данных.')
             return
-        print(f'Добавлено место "{new_place["title"]}".')
-        for image_num, image_url in enumerate(new_place['imgs']):
+        print(f'Добавлено место "{row_place["title"]}".')
+        for image_num, image_url in enumerate(row_place['imgs']):
             response = requests.get(image_url)
             response.raise_for_status()
             image_name = Path(urlparse(image_url).path).name
-            file_path = Path(MEDIA_ROOT).joinpath(image_name)
-            with open(file_path, 'wb') as file:
-                file.write(response.content)
+            image_file = ContentFile(response.content, name=image_name)
             image, image_created = Image.objects.get_or_create(
-                image=image_name,
+                image=image_file,
                 place_id=place.id,
                 defaults={'sequence_number': image_num, },
             )
