eb0cdca45339273f9070213c189bbe092ad4b16a
mulchus <mulch@list.ru>
2023-05-29 22:49:18 +0300

    fix misspell. dry code.

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -21,35 +21,32 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         response = requests.get(options['url_about_place'])
         response.raise_for_status()
-        row_place = response.json()
-        row_place_lng = row_place['coordinates']['lng']
-        row_place_lat = row_place['coordinates']['lat']
+        raw_place = response.json()
+        raw_place_lng = raw_place['coordinates']['lng']
+        raw_place_lat = raw_place['coordinates']['lat']
 
         place, place_created = Place.objects.get_or_create(
-            title=row_place['title'],
-            lng=row_place_lng,
-            lat=row_place_lat,
+            title=raw_place['title'],
+            lng=raw_place_lng,
+            lat=raw_place_lat,
             defaults={
-                'description_short': row_place.get('description_short', ''),
-                'description_long': row_place.get('description_long', '')
+                'description_short': raw_place.get('description_short', ''),
+                'description_long': raw_place.get('description_long', '')
             },
         )
 
         if not place_created:
-            print(f'Место "{row_place["title"]}" уже есть в базе данных.')
+            print(f'Место "{raw_place["title"]}" уже есть в базе данных.')
             return
-        print(f'Добавлено место "{row_place["title"]}".')
-        for image_num, image_url in enumerate(row_place.get('imgs', '')):
+        print(f'Добавлено место "{raw_place["title"]}".')
+        for image_num, image_url in enumerate(raw_place.get('imgs', [])):
             response = requests.get(image_url)
             response.raise_for_status()
             image_name = Path(urlparse(image_url).path).name
             image_file = ContentFile(response.content, name=image_name)
-            image, image_created = Image.objects.get_or_create(
+            image, image_created = Image.objects.create(
                 image=image_file,
                 place_id=place.id,
                 defaults={'sequence_number': image_num, },
             )
-            if not image_created:
-                print(f'Фотография "{image.image}" уже имеется в базе данных у данной локации.')
-            else:
-                print(f'Добавлена фотография "{image.image}".')
+            print(f'Добавлена фотография "{image.image}".')
